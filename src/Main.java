972fc8ed5716d42a313c4d1d644c68374cbe5dbb
zgvsk <zahaievskyi.oleksandr@chnu.edu.ua>
2025-09-14 14:39:43 +0300

    first comit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Lab 1 Java");
-        System.out.printf("test");
+        {
+            out.println("Завдання 1: ");
+            task1();
+            out.println("Завдання 2: ");
+            task2();
+            out.println("Завдання 3: ");
+            task3();
+            out.println("Завдання 4: ");
+            task4();
+        }
     }
+        public static void task1()
+        {
+
+        }
+        public static void task2()
+        {
+
+        }
+        public static void task3()
+        {
+
+        }
+        public static void task4()
+        {
+
+        }
 }
